db32aca23ac9f3e6dc507321b7d26ad579d87432
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2025-02-28 23:14:53 +0200

    Helmet Protection {Added}
    Most important points covered with the use of helmet to protect from uncovered PoweredBy tag on network and more critical points like injections on input areas through the app.

--- crowdfunding/src/main/resources/src (front-end)/serverInit.js
+++ crowdfunding/src/main/resources/src (front-end)/serverInit.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
 // App is using static the current directory
 app.use(express.static(__dirname));
+
+// Protecting critical points with helmet
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'DENY' }));
+app.use(helmet.dnsPrefetchControl({}));
+app.use(helmet.noSniff({}));
+app.use(helmet.ieNoOpen({}));
+app.use(helmet.xssFilter());
+app.use(helmet.hsts({}));
 
 // Server running on localhost port 7000
 const port = 7000;
